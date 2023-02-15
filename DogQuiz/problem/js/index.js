4aec3ead6671df1d490c412b53daac8d455e89ee
Adam <adam-cheaib@hotmail.com>
2023-02-15 17:32:43 +0100

    Adjustments

--- DogQuiz/problem/js/index.js
+++ DogQuiz/problem/js/index.js
@@ -1,1 +1,23 @@
 "use strict"
+
+/*
+Skapa designen först för varje sida genom att lägga alla HTML-element som behöver finnas och de som ej behöver synas
+och synas beroende på vilken class det är som aktiverat.
+
+
+
+Hur man ändrar CSS:
+function change_css(event) {
+    let new_css_source = document.createElement("link");
+    new_css_source.innerHTML = `<link rel="stylesheet" href="./css/index.css">`;
+    document.querySelector("head").appendChild(new_css_source);
+    document.querySelector("#haha").remove()
+};
+
+
+*/
+
+
+
+let random_dog = Math.floor(Math.random() * ALL_BREEDS.length);
+console.log(random_dog);
