8fd7f0396a7f262548e045e133a4f9b8783d8753
Adam <adam-cheaib@hotmail.com>
2023-02-16 01:22:10 +0100

    proper stuff added

--- DogQuiz/problem/js/test.js
+++ DogQuiz/problem/js/test.js
@@ -5,6 +5,9 @@ doggo.src = "./media/audio/background.mp3";
 
 function play_doggo() {
     doggo.play();
+    document.querySelector(".doggo").style.opacity = "100%";
+    document.querySelector(".doggo").style.transitionProperty = "opacity";
+    document.querySelector(".doggo").style.transitionDuration = "1s";
 }
 
 document.querySelector("#login_button").addEventListener("click", play_doggo);
