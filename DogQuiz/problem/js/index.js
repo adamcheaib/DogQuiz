2f0a3f226d13ea613d9424d7d55d5adb07c0ecc4
Adam <adam-cheaib@hotmail.com>
2023-02-24 14:59:07 +0100

    Comments removed.

--- DogQuiz/problem/js/index.js
+++ DogQuiz/problem/js/index.js
@@ -6,15 +6,8 @@ KVAR ATT GÖRA:
 - Ändra från en bakgrundsbild till en img-element så att storleken är dynamisk.
 */
 
-
-
-/*(Det där uppe) Kanske ta bort allt där uppe och spara till senare! */
-
 document.querySelector("#already_or_newAccount").addEventListener("click", css_register_login_change);
 
 document.querySelector("#login_button").addEventListener("click", accountCheck);
 
 document.querySelector(".play_click").addEventListener("click", click_to_play);
-
-// window.localStorage.clear();
-
